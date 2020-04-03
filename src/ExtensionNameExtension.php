27cfefa94ca1c96b87eb65cce6cd5682cbed399e
David Krause <krause@biochem2.uni-frankfurt.de>
2020-04-03 20:33:24 +0200

    init

---
+++ src/ExtensionNameExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\enthus1ast\pupmed;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * ExtensionName extension class.
+ *
+ * @author Your Name <you@example.com>
+ */
+class ExtensionNameExtension extends SimpleExtension
+{
+}
