87ef1d3facd56ee2252d45bb064157a1be939414
David Krause <krause@biochem2.uni-frankfurt.de>
2020-04-03 20:37:57 +0200

    init2

---
+++ src/PupMedExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\enthus1ast\PupMed;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * ExtensionName extension class.
+ *
+ * @author enthus1ast <david@code0.xyz>
+ */
+class PupMedExtension extends SimpleExtension
+{
+}
